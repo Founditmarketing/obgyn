1691aef5665846fa15e9528aea9e223f9d5f8cdb
Trevor Developer <trevor.developer@example.com>
2026-03-21 20:19:43 -0500

    Remove Price Transparency from public homepage

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import { DigitalDoula } from '@/components/DigitalDoula';
 import { AlexandriaConcierge } from '@/components/AlexandriaConcierge';
 import { ClinicTour } from '@/components/ClinicTour';
 import { Testimonials } from '@/components/Testimonials';
-import { PriceTransparency } from '@/components/PriceTransparency';
 
 import { ScrollReveal } from '@/components/visuals/ScrollReveal';
 
@@ -17,7 +16,6 @@ export default function Home() {
       <ScrollReveal><Testimonials /></ScrollReveal>
       <ScrollReveal><DigitalDoula /></ScrollReveal>
       <ScrollReveal><AlexandriaConcierge /></ScrollReveal>
-      <ScrollReveal><PriceTransparency /></ScrollReveal>
     </div>
   );
 }
